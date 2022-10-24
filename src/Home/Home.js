1ae9aff650b714ec9239b104e9b9be7424505ac5
RakibulHasan2 <rakibulhasan99445@gmail.com>
2022-10-24 13:33:47 +0600

    first commit

---
+++ src/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Carousels from '../Carousels/Carousels';
+
+const Home = () => {
+    return (
+        <div>
+           <Carousels></Carousels>
+        </div>
+    );
+};
+
+export default Home;
